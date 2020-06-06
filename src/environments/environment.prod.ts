97129e58961f07d076c61f6fd5612f55eaa9a6be
Arun Kumar <arunkumar7620@gmail.com>
2020-06-06 07:13:45 +0530

    removed all fiels except deployment files

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  chrbaseuri: 'http://ec2-13-127-77-144.ap-south-1.compute.amazonaws.com/chr/hospital'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  chrbaseuri: 'http://localhost:8080/chr/hospital'
-};
